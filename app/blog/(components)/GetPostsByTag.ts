56f2e1228a27a137d0e86178001a16ec18ae3182
hanad124 <hanaddaahir124@gmail.com>
2023-10-04 23:38:41 +0300

    Migrated posts by tag data from mark down file to strapi cms .

--- app/blog/(components)/GetPostsByTag.ts
+++ app/blog/(components)/GetPostsByTag.ts
@@ -1,35 +1,59 @@
+import axios from "axios";
 import fs from "fs";
 import matter from "gray-matter";
+import { IPost, IThumbnail, IAvator } from "@/types/postType";
 
-const getPostsByTag = (tag: string) => {
-  const postsDirectory = "posts"; // Directory where your markdown post files are stored
-  const fileNames = fs.readdirSync(postsDirectory);
+const getPostsByTag = async (tagg: string) => {
+  try {
+    // Get post data from Strapi
+    const response = await axios.get(
+      `http://127.0.0.1:1337/api/posts?populate=*`
+    );
 
-  const posts = fileNames
-    .map((fileName) => {
-      const fileContents = fs.readFileSync(
-        `${postsDirectory}/${fileName}`,
-        "utf-8"
-      );
-      const { data, content } = matter(fileContents);
+    const data = response.data.data;
 
-      // Check if the post has the specified tag
-      if (data.tags && data.tags.includes(tag)) {
-        return {
-          slug: fileName.replace(".md", ""),
-          title: data.title,
-          image: data.image,
-          category: data.category,
-          date: data.date,
-          author: data.author,
-          content, // Include the content of the post
-        };
-      }
-      return null;
-    })
-    .filter(Boolean);
+    // Filter posts by tag
+    const filteredPosts = data.filter(
+      (post: IPost) =>
+        post.attributes.tags.tags.map((t: string) => t).includes(tagg)
+      // )
+    );
+    // console.log("filteredPosts", filteredPosts);
 
-  return posts;
+    return filteredPosts;
+  } catch (error) {
+    console.error("Error fetching posts by tag:", error);
+    throw error;
+  }
 };
 
 export { getPostsByTag };
+
+// const postsDirectory = "posts"; // Directory where your markdown post files are stored
+// const fileNames = fs.readdirSync(postsDirectory);
+
+// const posts = fileNames
+//   .map((fileName) => {
+//     const fileContents = fs.readFileSync(
+//       `${postsDirectory}/${fileName}`,
+//       "utf-8"
+//     );
+//     const { data, content } = matter(fileContents);
+
+//     // Check if the post has the specified tag
+//     if (data.tags && data.tags.includes(tag)) {
+//       return {
+//         slug: fileName.replace(".md", ""),
+//         title: data.title,
+//         image: data.image,
+//         category: data.category,
+//         date: data.date,
+//         author: data.author,
+//         content, // Include the content of the post
+//       };
+//     }
+//     return null;
+//   })
+//   .filter(Boolean);
+
+// return posts;
--- app/blog/tag/[tag]/page.tsx
+++ app/blog/tag/[tag]/page.tsx
@@ -12,24 +12,30 @@ import { FiUser } from "react-icons/fi";
 import { FiCalendar } from "react-icons/fi";
 import { FiTag } from "react-icons/fi";
 import { Button } from "@/components/ui/button";
+import { IPost, IThumbnail, IAvator } from "@/types/postType";
+import axios from "axios";
 
-export const generateStaticParams = async () => {
-  const posts = getPostMetadata();
-  return posts.map((post) => ({
-    slug: post.slug,
-    params: { tag: post.slug },
-  }));
-};
+export async function getStaticPaths() {
+  const response = await axios.get("http://127.0.0.1:1337/api/posts");
+  const paths = response.data.data.map((post: IPost) => {
+    return { params: { tag: post.attributes.category } };
+  });
 
-const page = (props: any) => {
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const page = async (props: any) => {
   // Generate breadcrumb items
   const breadcrumbItems = [
     { label: "Home", href: "/" },
     { label: "Blog", href: "/blog" },
     { label: props.params.tag, href: `/blog/category/${props.params.tag}` },
   ];
-
-  const postTag = getPostsByTag(props.params.tag);
+  // console.log("PROPS:", props);
+  const postTag = await getPostsByTag(props.params.tag);
   const postTagLength = postTag.length;
   return (
     <>
@@ -45,58 +51,70 @@ const page = (props: any) => {
       <div className="mx-auto px-4  md:px-10 py-16  flex flex-col md:flex-row justify-center gap-x-5 gap-y-10 ">
         <div className="max-[450px]:w-[22rem] break-words sm:mx-4 ">
           <div className="mx-auto  flex flex-col justify-center gap-x-10 gap-y-10  ">
-            {postTag.map((post: any) => (
+            {postTag.map((post: IPost) => (
               <div
                 className="flex flex-col border rounded-md overflow-hidden md:max-w-3xl"
-                key={post.slug}
+                key={post.attributes.slug}
               >
                 <div className="relative w-full">
                   <Link
-                    href={`/blog/posts/${post.slug}`}
+                    href={`/blog/posts/${post.attributes.slug}`}
                     className=" underline-non"
                   >
-                    <Image
-                      src={post.image}
+                    {post.attributes.thumbnail.data.map(
+                      (thumbnail: IThumbnail) => (
+                        <Image
+                          src={`http://127.0.0.1:1337${thumbnail.attributes.url}`}
+                          alt={post.attributes.title}
+                          width={500}
+                          height={500}
+                          className=" w-full md:h-[24rem]"
+                        />
+                      )
+                    )}
+                  </Link>
+                  {/* <Image
+                      src={post.a.image}
                       alt={post.title}
                       width={500}
                       height={500}
                       className=" w-full md:h-[24rem]"
                     />
-                  </Link>
+                  </Link> */}
                   <Badge className="absolute -bottom-8 mx-auto md:-bottom-4 left-10 transform flex flex-col md:flex-row justify-start items-start md:items-center gap-3 py-2 font-normal text-sm hover:bg-custom_primary">
                     <div className="flex items-center gap-2">
                       <FiUser className="w-4 h-4 " />
-                      <span>{post.author}</span>
+                      <span>{post.attributes.author}</span>
                     </div>
                     <span className="hidden md:block">/</span>
                     <div className="flex items-center gap-2">
                       <FiCalendar className="w-4 h-4" />
-                      <span>{post.date}</span>
+                      <span>{post.attributes.date}</span>
                     </div>
                     <span className="hidden md:block">/</span>
                     <div className="flex items-center gap-2">
                       <FiTag className="w-4 h-4 rotate-45" />
-                      <span>{post.category}</span>
+                      <span>{post.attributes.category}</span>
                     </div>
                   </Badge>
                 </div>
                 <Link
-                  href={`/blog/posts/${post.slug}`}
+                  href={`/blog/posts/${post.attributes.slug}`}
                   className=" underline-non"
                 >
                   <h2 className="text-3xl font-bold underline-none  md:mt-10 text-custom_secondary px-4 md:px-10 md:py-0 py-16 ">
-                    {post.title}
+                    {post.attributes.title}
                   </h2>
                 </Link>
                 <article className=" md:my-7 prose md:prose-lg dark:prose-invert  prose-img:rounded prose-headings:text -mt-10 px-4 prose-headings:underline-none  md:px-10 md:py-0 pb-10  ">
                   <Markdown>
-                    {post.content.length > 200
-                      ? post.content.slice(0, 200) + "..."
-                      : post.content}
+                    {post.attributes.content.length > 200
+                      ? post.attributes.content.slice(0, 200) + "..."
+                      : post.attributes.content}
                   </Markdown>
                 </article>
                 <div className="inline px-4 md:px-10 mb-8 md:py-0 py-16 ">
-                  <Link href={`/blog/posts/${post.slug}`}>
+                  <Link href={`/blog/posts/${post.attributes.slug}`}>
                     <Button className="flex items-center gap-2 py-6 border bg-transparent text-custom_secondary hover:bg-custom_primary hover:text-background shadow-none">
                       <span>Continue Reading</span>
                       <span className="font-bold text-lg"> &rarr;</span>
